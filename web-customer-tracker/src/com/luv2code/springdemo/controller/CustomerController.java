530914e96c3459fa6541d79f7959e7e72c2c792d
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-19 15:07:11 -0400

    it was getting IGNORED!!!!!!

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,28 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
-	// need to inject the customer dao
+
+	// need to inject the customer Service which is using customer DAO
 	@Autowired
-	private CustomerDAO customerDAO;
-	
-	@RequestMapping("/list")
+	private CustomerService customerService;
+
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		
-		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
-		
+
+		// get customers from the customerService
+		List<Customer> theCustomers = customerService.getCustomers();
+
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
-		
+
 		return "list-customers";
+	}
+	
+	@GetMapping("showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
 	}
 }
