e92fffaa1a0c9bd5bae6b61512055a0024b15f93
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-28 18:54:11 -0400

    Adding @AfterReturning advice

--- web-customer-tracker/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -44,5 +45,14 @@ public class CRMLoggingAspect {
 	}
 	
 	// add @AfterReturning advice
-	
+	@AfterReturning(
+			pointcut = "forAppFlow()",
+			returning = "theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		// display the method we are calling
+				String theMethod = theJoinPoint.getSignature().toShortString();
+				myLogger.info("======>> in @AfterReturning: from method: " + theMethod);
+				myLogger.info("=====>> result: " + theResult);
+	}
 }
