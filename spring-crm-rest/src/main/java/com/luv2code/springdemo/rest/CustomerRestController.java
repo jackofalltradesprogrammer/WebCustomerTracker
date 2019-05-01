f3b952a8688241c8301bdf13c58d2214a434431a
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-01 11:21:56 -0400

    Add new customer with REST API using POST with POSTMAN

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,18 @@ public class CustomerRestController {
 		}
 
 		return customer;
+	}
+	
+	// add Mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// all just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+		
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
 	}
 
 }
